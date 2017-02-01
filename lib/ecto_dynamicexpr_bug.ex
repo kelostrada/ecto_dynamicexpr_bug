5aface19a42c7ae41a22ac4911d76581ad5c6ba6
kelostrada <kelostrada@gmail.com>
2017-02-01 14:16:43 +0100

    Init mix

---
+++ lib/ecto_dynamicexpr_bug.ex
@@ -0,0 +1,18 @@
+defmodule EctoDynamicexprBug do
+  @moduledoc """
+  Documentation for EctoDynamicexprBug.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> EctoDynamicexprBug.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ecto_dynamicexpr_bug_test.exs
@@ -0,0 +1,8 @@
+defmodule EctoDynamicexprBugTest do
+  use ExUnit.Case
+  doctest EctoDynamicexprBug
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
